92b4fde380368f6555b5ef8f139230ad462f903a
echo-escape <2237455044@qq.com>
2025-02-20 19:16:30 +0800

    完成lecture5

---
+++ DrawGraphics.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+public class DrawGraphics {
+    BouncingBox box;
+    ArrayList<BouncingBox> boxes;
+    /** Initializes this class for drawing. */
+    public DrawGraphics() {
+        box = new BouncingBox(200, 50, Color.RED);
+        boxes = new ArrayList<>();
+
+        boxes.add(new BouncingBox(50, 100, Color.blue));
+        boxes.add(new BouncingBox(100, 200, Color.RED));
+        boxes.add(new BouncingBox(75, 150, Color.BLACK));
+
+        for (BouncingBox boxSingle : boxes) {
+            boxSingle.setMovementVector(0, -2);
+        }
+    }
+
+    /** Draw the contents of the window on surface. Called 20 times per second. */
+    public void draw(Graphics surface) {
+        surface.drawLine(12, 50, 250, 250);
+        box.draw(surface);
+
+        for (BouncingBox boxSingle : boxes) {
+            boxSingle.draw(surface);
+        }
+    }
+} 
